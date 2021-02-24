0dd9c3874f8ecd4895ac8cc15091a9b02a849af0
FreddyFox1 <svchaykun@mail.ru>
2021-02-24 23:47:31 +0300

    Add controller for work with images

---
+++ Warehouse/Controllers/ImageResizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Controllers
+{
+    /// <summary>
+    /// Класс предназначен для обработки изображений загружаемых на сервер, 
+    /// а именно сжатие их и уменьшения разрешения для экономия места на 
+    /// сервер и быстрой загрузки их на мобильных устройствах
+    /// </summary>
+    public class ImageResizer
+    {
+
+    }
+}
