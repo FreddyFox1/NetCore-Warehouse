37551fd81caa2ede9cc199f0ac914b73fe276ee5
FreddyFox1 <svchaykun@mail.ru>
2021-02-27 02:36:42 +0300

    Add simple role initializer

---
+++ Warehouse/Controllers/RoleInitializer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Warehouse.Areas.Identity.Data;
+
+namespace Warehouse.Controllers
+{
+    public class RoleInitializer : Controller
+    {
+        /// <summary>
+        /// Создаем 4 типа пользователей
+        /// от админа до водителя
+        /// </summary>
+        /// <param name="roleManager">Сервис для работы с ролями пользователей приложения</param>
+        /// <returns></returns>
+        public static async Task InitializeAsync(RoleManager<IdentityRole> roleManager)
+        {
+            if (await roleManager.FindByNameAsync("Admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+            if (await roleManager.FindByNameAsync("User") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("User"));
+            }
+
+            if (await roleManager.FindByNameAsync("Watcher") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Watcher"));
+            }
+
+            if (await roleManager.FindByNameAsync("Driver") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Driver"));
+            }
+        }
+    }
+}
