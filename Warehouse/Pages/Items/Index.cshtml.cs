b4845f41d823496fe5f1d43a57c9e8c034a6c3f0
FreddyFox1 <svchaykun@mail.ru>
2021-02-27 02:37:08 +0300

    Add Items CRUD

---
+++ Warehouse/Pages/Items/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Warehouse.Areas.Identity.Data;
+
+namespace Warehouse.Pages.Items
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Warehouse.Areas.Identity.Data.ApplicationDbContext _context;
+
+        public IndexModel(Warehouse.Areas.Identity.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Item> Item { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Item = await _context.Items
+                .Include(i => i.Category).ToListAsync();
+        }
+    }
+}
