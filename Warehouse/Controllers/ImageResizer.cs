7e4e89450ea04d3356b08152a91f5a176e0c7d66
FreddyFox1 <svchaykun@mail.ru>
2021-03-03 22:57:51 +0300

    Class resize images for save space into server

--- Warehouse/Controllers/ImageResizer.cs
+++ Warehouse/Controllers/ImageResizer.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ImageMagick;
+using System.IO;
 
 namespace Warehouse.Controllers
 {
@@ -12,6 +10,37 @@ namespace Warehouse.Controllers
     /// </summary>
     public class ImageResizer
     {
+        #region Const
+        //Vars for icons
+        private const int s_size = 250;
+        private const int s_quality = 100;
+        //Vars for large images
+        private const int l_size = 600;
+        private const int l_quality = 75;
+        #endregion
+
+        public static void Resizer(string InputPath, string FileName, string File)
+        {
+            using (var photo = new MagickImage(File))
+            {
+                photo.AutoOrient();
+
+                photo.Resize(l_size, 0);
+                photo.Quality = l_quality;
+                photo.Strip();
+                var Photo = Path.Combine(InputPath, "Photo", FileName);
+                photo.Write(Photo);
+
+                photo.Resize(s_size, 0);
+                photo.Quality = s_quality;
+                photo.Strip();
+                var Icon = Path.Combine(InputPath, "Icons", FileName);
+                photo.Write(Icon);
+            }
+            System.IO.File.Delete(File);
+        }
+
 
     }
 }
+
