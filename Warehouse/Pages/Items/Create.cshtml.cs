ef4e6fdf04bcb385fe432ecc42de3ed4fb770239
FreddyFox1 <svchaykun@mail.ru>
2021-03-04 23:44:25 +0300

    Add create page

--- Warehouse/Pages/Items/Create.cshtml.cs
+++ Warehouse/Pages/Items/Create.cshtml.cs
@@ -1,44 +1,131 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Warehouse.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using Warehouse.Areas.Identity.Data;
 
 namespace Warehouse.Pages.Items
 {
+    [Authorize(Policy = "DriverArea")]
     public class CreateModel : PageModel
     {
-        private readonly Warehouse.Areas.Identity.Data.ApplicationDbContext _context;
+        private readonly UserManager<WarehouseUser> _userManager;
+        private IWebHostEnvironment _environment;
+        private readonly ApplicationDbContext _context;
 
-        public CreateModel(Warehouse.Areas.Identity.Data.ApplicationDbContext context)
+        #region VariableForItems
+        [BindProperty]
+        public string StorageValue { get; set; }
+        [BindProperty]
+        public IFormFile Image { get; set; }
+        [BindProperty]
+        public List<SelectListItem> ItemGroup { get; set; }
+        [BindProperty]
+        public int SelectedCategory { get; set; }
+        [BindProperty]
+        public Item Items { get; set; }
+        [BindProperty]
+        public string Creator { get; set; }
+        #endregion
+
+        public CreateModel(ApplicationDbContext context, UserManager<WarehouseUser> userManager, IWebHostEnvironment environment)
         {
+            _userManager = userManager;
             _context = context;
+            _environment = environment;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
-        ViewData["CategoryID"] = new SelectList(_context.ItemsCategories, "CategoryID", "CategoryName");
+            CreateSelectList();
+            IdentityUser UserName = await _userManager.GetUserAsync(User);
+            if (UserName == null)
+            {
+                Creator = "Unknown user";
+            }
+            else Creator = User.Identity.Name;
             return Page();
         }
 
-        [BindProperty]
-        public Item Item { get; set; }
+        public void CreateSelectList()
+        {
+            ItemGroup =  _context.ItemsCategories.Select(a =>
+                       new SelectListItem
+                       {
+                           Value = a.CategoryName.ToString(),
+                           Text = a.CategoryName
+                       }).ToList();
+        }
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            if (!String.IsNullOrEmpty(StorageValue))
+                if (ValidArticle(Items.ItemArticle))
+                {
+                    Items.DateTransferItem = DateTime.Now;
+
+                    if (Image != null)
+                    {
+                        var uniqueFileName = Guid.NewGuid().ToString() + "_" + Image.FileName;
+                        var pathFile = Path.Combine(_environment.WebRootPath, "img");
+                        var file = Path.Combine(pathFile, uniqueFileName);
+                        //Save on server file
+                        using (var fileStream = new FileStream(file, FileMode.Create))
+                        {
+                            await Image.CopyToAsync(fileStream);
+                        }
+                        //Compress image
+                        ImageResizer.Resizer(pathFile, uniqueFileName, file);
+                        //Save FileName in DB
+                        Items.ItemPhoto = uniqueFileName;
+                    }
+                    else
+                    {
+                        Items.ItemPhoto = "noimage.jpg";
+                    }
+
+                    Items.CategoryID = SelectedCategory;
+                    Items.Creator = Creator;
+                    Items.ItemStorageID = StorageValue;
+
+                    _context.Items.Add(Items);
+                    await _context.SaveChangesAsync();
+                    return RedirectToPage("./Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Указанный артикул уже существует");
+                    CreateSelectList();
+                    return Page();
+                }
+            else
             {
+                ModelState.AddModelError("", "Склад не указан");
+                CreateSelectList();
                 return Page();
             }
+        }
 
-            _context.Items.Add(Item);
-            await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+        public bool ValidArticle(string Article)
+        {
+            var temp = _context.Items.FirstOrDefault(a => a.ItemArticle == Article);
+            if (temp == null)
+            {
+                return true;
+            }
+            else return false;
         }
     }
 }
