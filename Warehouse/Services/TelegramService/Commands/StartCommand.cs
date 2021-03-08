c8b41f1270747ba5dabdc5569686dc532ba0c0e6
FreddyFox1 <svchaykun@mail.ru>
2021-03-09 00:17:43 +0300

    Add telegram сommand handler

---
+++ Warehouse/Services/TelegramService/Commands/StartCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Warehouse.Services.TelegramService.Commands
+{
+    public class StartCommand : Command
+    {
+        public override string Name => "start";
+
+        public async override void Execute(Message message, TelegramBotClient client)
+        {
+            var ChatId = message.Chat.Id;
+            var MessageId = message.MessageId;
+            await client.SendTextMessageAsync(ChatId, "Start...", replyToMessageId: MessageId);
+        }
+    }
+}
