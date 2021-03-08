9dc0f96518eb8e8a16074758e5070bbc09c972f9
FreddyFox1 <svchaykun@mail.ru>
2021-03-08 18:29:52 +0300

    initial TestProject

---
+++ WarehouseTests/TelegramServiceTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace WarehouseTests
+{
+    [TestClass]
+    public class TelegramServiceTests
+    {
+        [TestMethod]
+        public void TelegramServiceTest()
+        {
+           
+
+        }
+    }
+}
