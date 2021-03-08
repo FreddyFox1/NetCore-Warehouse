9cf646c8c1d60861970026385e704056b6199497
FreddyFox1 <svchaykun@mail.ru>
2021-03-08 14:05:27 +0300

    Update namespace

--- Warehouse/Controllers/ItemsController.cs
+++ Warehouse/Controllers/ItemsController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Warehouse.Areas.Identity.Data;
+using Warehouse.Model;
 
 namespace Warehouse.Controllers
 {
--- Warehouse/Controllers/RoleInitializer.cs
+++ Warehouse/Controllers/RoleInitializer.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Warehouse.Areas.Identity.Data;
 
 namespace Warehouse.Controllers
 {
