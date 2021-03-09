fe75e3909fcab2e521b5bcbf1258b4554610e697
FreddyFox1 <svchaykun@mail.ru>
2021-03-10 01:14:09 +0300

    Add Bitrix24Service

---
+++ Warehouse/Services/Bitrix24Service/BitrixAbstractions/IBitrix.cs
@@ -0,0 +1,10 @@
+﻿using Warehouse.Model;
+
+namespace Warehouse.Services.Bitrix24Service.BitrixAbstractions
+{
+    interface IBitrix
+    {
+        void CreateTask(Item item);
+        void PushTask();
+    }
+}
