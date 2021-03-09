bd363f51e8a16ea2f1a50077b9306fe82d15c79c
FreddyFox1 <svchaykun@mail.ru>
2021-03-09 23:02:43 +0300

    Add Testlib

---
+++ Warehouse.Tests/RouteTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Warehouse.Tests
+{
+    public class RouteTests
+    : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public RouteTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/items")]
+        [InlineData("/items/Create")]
+        [InlineData("/items/Move")]
+        [InlineData("/items/Eventlog")]
+        [InlineData("/admin/telegram")]
+        [InlineData("/admin/roles")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
