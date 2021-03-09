009123ee8060b44d1422b07d2a3acf3e62c5a578
FreddyFox1 <svchaykun@mail.ru>
2021-03-09 23:51:33 +0300

    Add custom factory

---
+++ Warehouse.Tests/IntegrationTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Warehouse.Model;
+
+namespace Warehouse.Tests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+        protected readonly WebApplicationFactory<Startup> factory;
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(ApplicationDbContext));
+                        services.AddDbContext<ApplicationDbContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+
+            factory = appFactory;
+            TestClient = appFactory.CreateClient();
+            //appFactory.Services.GetService<ApplicationDbContext>();
+        }
+
+        protected void GetContext(Action<ApplicationDbContext> test)
+        {
+            using (var scope = factory.Server.Host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                test(context);
+            }
+        }
+
+        protected ApplicationDbContext GetContext()
+        {
+            //using (var scope = factory.Server.Host.Services.CreateScope())
+            //{
+            var context = factory.Services.GetService<ApplicationDbContext>();// ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                return context;
+            
+        }
+
+    }
+}
