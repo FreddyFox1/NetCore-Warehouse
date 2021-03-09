e29baccfa92c76c25034018df3b9ba01cbd27c04
FreddyFox1 <svchaykun@mail.ru>
2021-03-10 01:15:47 +0300

    Add Bitrix Service

---
+++ Warehouse/Services/Bitrix24Service/BitrixKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Services.Bitrix24Service
+{
+    /// <summary>
+    /// Ключ авторизации для работы с API Bitrix24
+    /// </summary>
+    public class BitrixKey
+    {
+        public string AuthKey { get; set; }
+    }
+}
---
+++ Warehouse/Services/Bitrix24Service/BitrixService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Warehouse.Model;
+using Warehouse.Services.Bitrix24Service.BitrixAbstractions;
+
+namespace Warehouse.Services.Bitrix24Service
+{
+    /// <summary>
+    /// Сервис для работы с API Bitrix24
+    /// </summary>
+    public class BitrixService : IBitrix
+    {
+        /// <summary>
+        /// Создаем задачу на основе добавленного Item'a
+        /// </summary>
+        /// <param name="item">Передаем новый добавленный объект</param>
+        public void CreateTask(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Отправляем сформированную задачу на сервер Bitrix24
+        /// </summary>
+        public void PushTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
