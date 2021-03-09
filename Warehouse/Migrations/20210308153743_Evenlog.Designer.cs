f8476607facd5a8d0207ac204d609d493a1c7d44
FreddyFox1 <svchaykun@mail.ru>
2021-03-09 23:54:31 +0300

    Some changes

--- Warehouse/Migrations/20210308153743_Evenlog.Designer.cs
+++ Warehouse/Migrations/20210308153743_Evenlog.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Warehouse.Model;
 
 namespace Warehouse.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210308153743_Evenlog")]
+    partial class Evenlog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Warehouse/Migrations/20210308153743_Evenlog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Warehouse.Migrations
+{
+    public partial class Evenlog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ItemLogs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LogItemName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LogUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LogOldStorage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LogCurStorage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LogItemArticle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LogDateTransfer = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemLogs", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemLogs");
+        }
+    }
+}
