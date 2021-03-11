b37dbe3a0662318cfc4b6fb117fd8cab514d8dca
FreddyFox1 <svchaykun@mail.ru>
2021-03-11 22:01:52 +0300

    Add BitrixServiceTests

---
+++ Warehouse.Tests/BitrixServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Model;
+using Warehouse.Services.Bitrix24Service;
+using Xunit;
+
+namespace Warehouse.Tests
+{
+    public class BitrixServiceTests : IntegrationTest
+    {
+
+        Item item = new Item()
+        {
+            ItemID = 1,
+            ItemName = "TestItem",
+            ItemArticle = "ZMF-130-222-333"
+        };
+
+        [Fact]
+        public void CreateTaskTest_return_string_with_article()
+        {
+            BitrixService bitrixService = new BitrixService(GetBitrixKeysOptions());
+            var expected = item.ItemArticle;
+            var actual = bitrixService.CreateTask(item);
+            Assert.Contains(expected, actual);
+        }
+
+        [Fact]
+        public void PushTask_return_True()
+        {
+            BitrixService bitrixService = new BitrixService(GetBitrixKeysOptions());
+            var str = bitrixService.CreateTask(item);
+            var actual = bitrixService.PushTask(str, item.ItemArticle);
+
+            Assert.True(actual);
+        }
+    }
+}
