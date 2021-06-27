69d78f52bb117c71630452bdf34eac55b0629bfb
Svyatoslav <s.chaikun@bars-open.ru>
2021-06-27 20:30:13 +0300

    Add Tests for Integrator

---
+++ Warehouse.Tests/ServicesTests/IntegratorTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Warehouse.Services.Integrator;
+using System.IO;
+using System;
+
+namespace Warehouse.Tests.ServicesTests
+{
+    public class IntegratorTest : IntegrationTest
+    {
+        [Fact]
+        public void CategoryTable_input_json_return_objects()
+        {
+            var json = "{\"Categories\": [{\"IDCategory\" : 1,\"CategoryName\" : \"Тротуарная плитка\"},{\"IDCategory\" : 2,\"CategoryName\" : \"Брусчатка\"}]}";
+            IntegratorService IntegratorService = new IntegratorService();
+            var objectsCategory = IntegratorService.CategoryTable(json);
+            var assert = objectsCategory.Count > 0;
+            Assert.True(assert);
+        }
+    }
+}
