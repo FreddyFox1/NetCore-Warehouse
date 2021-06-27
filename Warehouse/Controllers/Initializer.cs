d8ca7cf5407738746f075f52cc5e10bec5402529
FreddyFox <svchaykun@mail.ru>
2021-06-27 23:28:09 +0300

    Add initial user for first start app

--- Warehouse/Controllers/Initializer.cs
+++ Warehouse/Controllers/Initializer.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace Warehouse.Controllers
 {
-    public class RoleInitializer : Controller
+    public class Initializer : Controller
     {
         /// <summary>
         /// Создаем 4 роли пользователя
@@ -31,6 +31,28 @@ namespace Warehouse.Controllers
             {
                 await roleManager.CreateAsync(new IdentityRole("Driver"));
             }
+        }
+
+        public static async Task InitializeRootUser(UserManager<Model.WarehouseUser> _userManager)
+        {
+            Warehouse.Model.WarehouseUser root = new Model.WarehouseUser
+            {
+                UserName = "root@admin.ru",
+                Email = "root@admin.ru",
+                EmailConfirmed = true,
+            };
+
+            var result = await _userManager.CreateAsync(root, "root1234");
+            if (result.Succeeded)
+            {
+                var user1 = await _userManager.FindByEmailAsync(root.Email);
+                if (user1 != null)
+                {
+                    var user = await _userManager.FindByEmailAsync(root.Email);
+                    await _userManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+            
         }
     }
 }
